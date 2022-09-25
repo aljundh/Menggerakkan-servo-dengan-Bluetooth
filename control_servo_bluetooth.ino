57ab44c58348134713cc5486b0b069bc5b594aee
Hafis Royhan <107126204+aljundh@users.noreply.github.com>
2022-09-25 15:05:57 +0700

    Add files via upload

---
+++ control_servo_bluetooth.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+SoftwareSerial BT(0,1); //RX,TX
+Servo servo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  BT.begin(9600);
+  servo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(BT.available()> 0)
+{
+  int servopos = BT.read();
+  Serial.println(servopos);
+  servo.write(servopos);
+}
+}
